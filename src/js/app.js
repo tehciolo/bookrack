ec3ffba2461efd9f08fda1a7afd61c819355ae33
Cosmin Cioacla <tehciolo@gmail.com>
2016-03-21 00:43:55 +0200

    add vue router

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,85 @@
 var Vue = require('vue');
+var VueRouter = require('vue-router');
 
-new Vue({
-  el: '#app',
 
-  data: {
+var siteData = require('./siteData.js');
+// var pageData = require('./pageData.js');
 
+Vue.use(VueRouter);
+//Vue.config.debug = true;
+
+
+
+// Define menu component
+Vue.component('menu', {
+  //props: ['data'],
+
+  template: '#menu-template',
+
+  data: function() {
+    return {
+      site: siteData()
+    }
   }
 });
+
+// Define book component
+var book = Vue.extend({
+  name: 'book',
+
+  template: '#book-template',
+
+  props: ['duo'],
+
+  computed: {
+    left: function() {
+      var playdoh = this.duo;
+      playdoh = playdoh.split('-');
+      return playdoh[0];
+    },
+
+    right: function() {
+      var playdoh = this.duo;
+      playdoh = playdoh.split('-');
+      return playdoh[1];
+    }
+  },
+
+  data: function() {
+    return {
+
+    }
+  }
+});
+
+// Define page component
+Vue.component('page', {
+  template: '#page-template',
+
+  props: ['data', 'type', 'src']
+});
+
+// Main Vue Instance
+// var App = new Vue({
+//   el: '#app',
+//
+//   data: {
+//     site: siteData()
+//   }
+// });
+
+// Set up router
+var App = Vue.extend({
+  data: function() {
+    return {
+      site: siteData()
+    }
+  }
+});
+var router = new VueRouter();
+router.map({
+  '/pages/*duo': {
+    component: book
+  }
+});
+router.start(App, '#app');
